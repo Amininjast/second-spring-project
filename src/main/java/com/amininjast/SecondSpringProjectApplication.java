b1af1400895bfc7098e03de62ea3306837dcd1de
Amininjast <aminarad228@gmail.com>
2023-09-21 23:58:38 +0330

    create default value every time app running.

--- src/main/java/com/amininjast/SecondSpringProjectApplication.java
+++ src/main/java/com/amininjast/SecondSpringProjectApplication.java
@@ -1,7 +1,13 @@
 package com.amininjast;
 
+import com.amininjast.library.Book;
+import com.amininjast.library.LibraryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SecondSpringProjectApplication {
@@ -10,4 +16,19 @@ public class SecondSpringProjectApplication {
 		SpringApplication.run(SecondSpringProjectApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(LibraryRepository libraryRepository) {
+		return args -> {
+			Book book1 = new Book(
+					1,
+					"Book1"
+			);
+			Book book2 = new Book(
+					2,
+					"Book2"
+			);
+			List<Book> books = List.of(book1, book2);
+			libraryRepository.saveAll(books);
+		};
+	}
 }
